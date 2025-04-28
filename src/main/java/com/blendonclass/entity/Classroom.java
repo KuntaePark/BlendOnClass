3f5048d2599f55147176bda24d159f10233fe91f
KuntaePark <sgvelcro@naver.com>
2025-04-28 14:44:49 +0900

    init

--- src/main/java/com/blendonclass/entity/Classroom.java
+++ src/main/java/com/blendonclass/entity/Classroom.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Entity
 @Getter@Setter
