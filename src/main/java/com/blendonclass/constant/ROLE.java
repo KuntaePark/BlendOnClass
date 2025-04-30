15019fdb09fdb436b5e31c8d4c64b90c34c0c49f
KuntaePark <sgvelcro@naver.com>
2025-04-30 17:52:39 +0900

    20250430-3

--- src/main/java/com/blendonclass/constant/ROLE.java
+++ src/main/java/com/blendonclass/constant/ROLE.java
@@ -1,5 +1,17 @@
 package com.blendonclass.constant;
 
+import lombok.Getter;
+
+@Getter
 public enum ROLE {
-    STUDENT, TEACHER, ADMIN
+    STUDENT("ROLE_STUDENT"),
+    TEACHER("ROLE_TEACHER"),
+    ADMIN("ROLE_ADMIN");
+
+    private final String role;
+    private ROLE(final String role) {
+        this.role = role;
+    }
+
+
 }
